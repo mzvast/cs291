139a5d378d52b85f231ee4fcae4de51baed41ae1
mzvast <mzvast@gmail.com>
2022-10-25 00:21:44 +0800

    unit8 ps-specular-mapping

--- unit8/ps-specular-mapping.js
+++ unit8/ps-specular-mapping.js
@@ -12,7 +12,8 @@ var teapotSize = 400;
 function createMaterial() {
 	// MATERIALS
 	// Student: use the texture '/media/img/cs291/textures/water.jpg'
-	var material = new THREE.MeshPhongMaterial( { shininess: 50 } );
+	var myTexture = THREE.ImageUtils.loadTexture('/media/img/cs291/textures/water.jpg');
+	var material = new THREE.MeshPhongMaterial( { shininess: 50,specularMap:myTexture } );
 	material.color.setHSL( 0.09, 0.46, 0.2 );
 	material.ambient.copy( material.color );
 	material.specular.setHSL( 0.09, 0.46, 1.0 );
