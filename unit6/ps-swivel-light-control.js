29f2f6f494aeabaef600908d7dbef9f03363142b
mzvast <mzvast@gmail.com>
2022-10-24 17:18:57 +0800

    unit6 ps-swivel-light-control

--- unit6/ps-swivel-light-control.js
+++ unit6/ps-swivel-light-control.js
@@ -256,6 +256,11 @@ function animate() {
 function render() {
 	var delta = clock.getDelta();
 	cameraControls.update(delta);
+
+	// 平行光，设置方向向量
+	light.position.x = Math.cos(effectController.angle * Math.PI / 180);
+	light.position.z = Math.sin(effectController.angle * Math.PI / 180);
+
 	renderer.render(scene, camera);
 }
 
