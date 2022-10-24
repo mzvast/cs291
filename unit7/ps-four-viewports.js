cf6a964eb820bf40be869bb820ed3cb1652dd9f0
mzvast <mzvast@gmail.com>
2022-10-24 22:01:28 +0800

    unit7 ps-four-viewports

--- unit7/ps-four-viewports.js
+++ unit7/ps-four-viewports.js
@@ -244,6 +244,19 @@ function init() {
 	// set X to be the up axis
 	topCam.up.set( 1, 0, 0 );
 
+	frontCam = new THREE.OrthographicCamera(
+		-aspectRatio*viewSize / 2, aspectRatio*viewSize / 2,
+		viewSize / 2, -viewSize / 2,
+		-1000, 1000 );
+	// set Y to be the up axis
+	frontCam.up.set(0,1,0);
+
+	sideCam = new THREE.OrthographicCamera(
+		-aspectRatio*viewSize / 2, aspectRatio*viewSize / 2,
+		viewSize / 2, -viewSize / 2,
+		-1000, 1000 );
+	sideCam.up.set(0,1,0);
+
 	// CONTROLS
 	cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
 	cameraControls.target.set(0,310,0);
@@ -288,6 +301,18 @@ function render() {
 	renderer.setViewport( 0.5*canvasWidth, 0.5*canvasHeight, 0.5*canvasWidth, 0.5*canvasHeight );
 	renderer.render( scene, topCam );
 
+
+	frontCam.position.copy( cameraControls.target );
+	frontCam.position.x -=1 ;
+	frontCam.lookAt( cameraControls.target );
+	renderer.setViewport( 0, 0.5*canvasHeight, 0.5*canvasWidth, 0.5*canvasHeight );
+	renderer.render(scene, frontCam);
+
+	sideCam.position.copy( cameraControls.target );
+	sideCam.position.z +=1;
+	sideCam.lookAt( cameraControls.target );
+	renderer.setViewport( 0.5*canvasHeight,0, 0.5*canvasWidth, 0.5*canvasHeight );
+	renderer.render(scene, sideCam);
 }
 
 try {
